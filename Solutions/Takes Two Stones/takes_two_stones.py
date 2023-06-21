3fd2c1e0f2f03f141e3f2117693acaeea43d4f40
armaankhabra <a.khabra5@gmail.com>
2023-06-20 21:47:33 -0600

    Add problem Takes Two Stones

---
+++ Solutions/Takes Two Stones/takes_two_stones.py
@@ -0,0 +1,13 @@
+# The problem can be found here: https://open.kattis.com/problems/twostones.
+
+def main():
+    n = int(input())
+
+    if n % 2 == 0:
+        print("Bob")
+    else:
+        print("Alice")
+
+
+if __name__ == "__main__":
+    main()
